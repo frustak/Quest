6db3a1353522058cbd3f6ec68ac7727258eeb9c8
frustak <soulburn007@gmail.com>
2021-03-18 17:04:17 +0330

    fix file creation for first time reading

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,5 +1,5 @@
 use quest::{DynResult, Quest, QuestList};
-use std::io;
+use std::{fs, io, path::Path};
 
 /// Saved quests file path
 static FILE_PATH: &str = "./quests.toml";
@@ -8,14 +8,18 @@ static FILE_PATH: &str = "./quests.toml";
 pub fn save_quests(quests: &[Quest]) -> DynResult {
     let quests = &QuestList::new(quests);
     let stringified_quests = toml::to_string(quests)?;
-    std::fs::write(FILE_PATH, stringified_quests)?;
+    fs::write(FILE_PATH, stringified_quests)?;
 
     Ok(())
 }
 
 /// Load all saved quests from file
 pub fn load_quests() -> Result<Vec<Quest>, io::Error> {
-    let stringified_quests = std::fs::read_to_string(FILE_PATH)?;
+    if !Path::new(FILE_PATH).exists() {
+        fs::File::create(FILE_PATH)?;
+    }
+
+    let stringified_quests = fs::read_to_string(FILE_PATH)?;
     let quest_list: QuestList = toml::from_str(&stringified_quests).unwrap_or_default();
 
     Ok(quest_list.quests)
