e7ab8189fc71d0e0b852fbe830a0506425b9cb0b
frustak <soulburn007@gmail.com>
2021-03-19 14:40:49 +0330

    add proper data path handling

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,25 +1,30 @@
+use lazy_static::lazy_static;
 use quest_tui::{DynResult, Quest, QuestList};
-use std::{fs, io, path::Path};
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
 
-/// Saved quests file path
-static FILE_PATH: &str = "./quests.toml";
+lazy_static! {
+    static ref FILE_PATH: PathBuf = dirs::data_dir().unwrap().join("quests.toml");
+}
 
 /// Save all quests to a file
 pub fn save_quests(quests: &[Quest]) -> DynResult {
     let quests = &QuestList::new(quests);
     let stringified_quests = toml::to_string(quests)?;
-    fs::write(FILE_PATH, stringified_quests)?;
+    fs::write(FILE_PATH.as_path(), stringified_quests)?;
 
     Ok(())
 }
 
 /// Load all saved quests from file
 pub fn load_quests() -> Result<Vec<Quest>, io::Error> {
-    if !Path::new(FILE_PATH).exists() {
-        fs::File::create(FILE_PATH)?;
+    if !Path::new(FILE_PATH.as_path()).exists() {
+        fs::File::create(FILE_PATH.as_path())?;
     }
 
-    let stringified_quests = fs::read_to_string(FILE_PATH)?;
+    let stringified_quests = fs::read_to_string(FILE_PATH.as_path())?;
     let quest_list: QuestList = toml::from_str(&stringified_quests).unwrap_or_default();
 
     Ok(quest_list.quests)
